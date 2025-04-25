a695e47ae7741edcf9508c0a320c9b5fdbe7c075
Elizabeth Njuguna <elizabethnjuguna217@gmail.com>
2025-04-25 13:06:57 +0300

    adds about

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,14 @@ function Home() {
   );
 }
 
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 {/* write an <About> component here */}
 
 
@@ -25,6 +33,9 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+       <Home/>
+       <About/>
+    
     </div>
   );
 }
